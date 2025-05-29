21c9a750ca48e1e920f996a09a70fc3060a48e41
b0mjur <vitalii.k.usa@gmail.com>
2025-05-28 17:21:02 -0700

    Add scrollable message list functionality on branch vitalii

---
+++ src/assets/MessageList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './MessageList.css';
+
+const MessageList = () => {
+  return (
+    <div className="message-list">
+      <div className="message left">Message 1</div>
+      <div className="message right">Message 2</div>
+      <div className="message left">Message 3</div>
+      <div className="message right">Message 4</div>
+      <div className="message left">Message 5</div>
+      <div className="message right">Message 6</div>
+      <div className="message left">Message 7</div>
+      <div className="message right">Message 8</div>
+      <div className="message left">Message 9</div>
+      <div className="message right">Message 10</div>
+    </div>
+  );
+};
+
+export default MessageList;
